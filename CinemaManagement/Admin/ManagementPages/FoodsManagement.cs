0949aaadb7db36d55998330e88c2e176c072f1e3
Phóng Ngô Văn <19520834@ms.uit.edu.vn>
2020-11-06 10:39:57 +0700

    Design Movie Manager

--- CinemaManagement/Admin/ManagementPages/FoodsManagement.cs
+++ CinemaManagement/Admin/ManagementPages/FoodsManagement.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CinemaManagement
+namespace CinemaManagement.Admin.ManagementPages
 {
-    public partial class Form1 : Form
+    public partial class FoodsManagement : BaseMangementPage
     {
-        public Form1()
+        public FoodsManagement()
         {
             InitializeComponent();
         }
